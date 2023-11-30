a2f4f635221663f664287f3a9f1401ee3222ee03
eduardoquintero117 <31216906+eduardoquintero117@users.noreply.github.com>
2023-11-30 07:27:48 +0000

    add data and configuration with copilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Eduardo Quintero Juan",
+  title: "iOS AR/VR Developer ",
+  email: "eduardoquiju@hotmail.com",
+  gitHub: "eduardoquintero117",
+  instagram: "eqdeveloper",
+  linkedIn: "eduardo-quintero-juan-b1a09923a/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "@eduard_Quin1",
+  youTube: "@eqdeveloper276?si=S6JZMp-0LWx1ICLo",
 };
 
 const primaryColor = "#4E567E";
